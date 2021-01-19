e96e43cd26333647ad0fa8a0d62ce6fe7189c201
aguzmant103 <67167307+aguzmant103@users.noreply.github.com>
2021-01-18 19:40:44 -0600

    Modifying ticker symbol

--- app_1_simple_stock_price/myapp2.py
+++ app_1_simple_stock_price/myapp2.py
@@ -4,23 +4,39 @@ import streamlit as st
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock **closing price** and ***volume*** of Google!
+#Shown are the stock **closing price** and ***volume*** of NATI!
 
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+
+
+input_ticker = "NATI"
+tickerSymbol = st.text_area("Input Ticker",input_ticker,height=50)
+st.header('Input Ticker')
+tickerSymbol
+
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2020-1-31', end='2020-5-31')
+
+tickerData2 = yf.Ticker("TSLA")
+tickerDf2 = tickerData2.history(period='1d', start='2020-1-31', end='2020-5-31')
+st.line_chart(tickerDf2.Close)
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
+# REMOVE COMMENT tickerData.recommendations
+
+
 
 st.write("""
 ## Closing Price
 """)
 st.line_chart(tickerDf.Close)
+st.write("""
+The data above show us the fragility of the stock market and the volatility that has characterized recent times.
+""")
 st.write("""
 ## Volume Price
 """)
